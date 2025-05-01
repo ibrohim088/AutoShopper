8031d4ab8fa95b130c435b2fb55e78070b456e03
ibrohim008 <sunnat5809@gmail.com>
2025-05-02 00:43:23 +0500

    update

--- src/schema/Product.js
+++ src/schema/Product.js
@@ -1,51 +1,51 @@
-// // src/schema/User.js
-// import mongoose from 'mongoose';
-// import bcrypt from 'bcryptjs';
+// src/schema/User.js
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
-// const userSchema = new mongoose.Schema({
-//   username: { type: String, required: true, unique: true, trim: true },
-//   fullname: { type: String, required: true },
-//   email: { type: String, lowercase: true, required: true, unique: true },
-//   // password: { type: String, required: true },
-//   password: {
-//     type: String,
-//     required: true,
-//     validate: {
-//       validator: function (value) {
-//         return /^\d{8}$/.test(value); // exactly 8 digits
-//       },
-//       message: 'Password must be exactly 8 digits.',
-//     },
-//   },
-//   role: {
-//     type: String,
-//     enum: ['CEO', 'Superadmin', 'Admin'],
-//     default: 'Admin',
-//   },
-//   isActive: { type: Boolean, default: true },
-//   // createdAt: { type: Date, default: Date.now },
-// }, {
-//   timestamps: {
-//     createdAt: "created_at",
-//     updatedAt: "updated_at",
-//   },
-//   versionKey: false,
-// });
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true, trim: true },
+  fullname: { type: String, required: true },
+  email: { type: String, lowercase: true, required: true, unique: true },
+  // password: { type: String, required: true },
+  password: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (value) {
+        return /^\d{8}$/.test(value); // exactly 8 digits
+      },
+      message: 'Password must be exactly 8 digits.',
+    },
+  },
+  role: {
+    type: String,
+    enum: ['CEO', 'Superadmin', 'Admin'],
+    default: 'Admin',
+  },
+  isActive: { type: Boolean, default: true },
+  // createdAt: { type: Date, default: Date.now },
+}, {
+  timestamps: {
+    createdAt: "created_at",
+    updatedAt: "updated_at",
+  },
+  versionKey: false,
+});
 
-// userSchema.pre('save', async function (next) {
-//   if (!this.isModified('password')) return next();
-//   try {
-//     const salt = await bcrypt.genSalt(10);
-//     this.password = await bcrypt.hash(this.password, salt);
-//     next();
-//   } catch (err) {
-//     next(err);
-//   }
-// });
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
 
-// userSchema.methods.comparePassword = async function (candidatePassword) {
-//   return bcrypt.compare(candidatePassword, this.password);
-// };
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
 
-// const User = mongoose.model('User', userSchema);
-// export default User;
+const User = mongoose.model('User', userSchema);
+export default User;
